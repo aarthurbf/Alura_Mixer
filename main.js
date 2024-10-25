4107898212ddee79de9a19e5fd20512cf6a1029b
Arthur Bobadilla <arthurbobadilla2006@gmail.com>
2024-10-24 23:12:25 -0300

    Segundo commit

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// Cria função de tocar o som
 function tocaSom(seletorAudio){
     const elemento = document.querySelector(seletorAudio);
+    // Verifica se o elemento realmente existe
     if (elemento && elemento.localName === 'audio'){
         elemento.play();
     }
@@ -9,25 +11,33 @@ function tocaSom(seletorAudio){
     
 }
 
+// Atribui o listaDeTeclas a todos os items com o valor .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+// Usa o for para percorrer a lista de items
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
+    // Definindo variaveis
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
+    // Função para reproduzir o audio no click
     tecla.onclick = function(){
         tocaSom(idAudio)
     }
 
+    // Função para quando a tecla for pressiona
     tecla.onkeydown = function (evento){
         if (evento.code == 'Space' || evento.code == 'Enter'){
+            // Adiciona a classe do css 'ativa'
             tecla.classList.add('ativa');
         }
     }
 
+    // Função para quando a tecla for solta
     tecla.onkeyup = function(){
+        // Remove a classe do css 'ativa'
         tecla.classList.remove('ativa');
     }
 }
